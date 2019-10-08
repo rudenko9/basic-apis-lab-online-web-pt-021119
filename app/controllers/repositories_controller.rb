45e6d95979efc65a9bb2e6e3f290a8d3f08ac613
Nataliya R <n.rudenko@gmail.com>
2019-10-08 00:13:09 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['q'] = params[:query]
+      end
+      
+    body = JSON.parse(resp.body)
+    if resp.success?
+      @repos = body['items']
+    else 
+      @erros = body['errors'][0]['code']
+    end 
+    rescue Faraday::ConnectionFailed
+      @error = "Timeout"
+    end
+    render 'search'
   end
 end
